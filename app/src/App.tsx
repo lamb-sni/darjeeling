d18fafc48784056519c3f263a38d862d4fcc283e
fumi.sni <fumi.shini@bizreach.co.jp>
2020-08-17 17:38:47 +0900

    簡単なサンプル作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import ShowMessage from "./pages/ShowMessage";
 import './App.css';
+
+/**
+ * 概要
+ * compnentディレクトリにMessageという渡されたstringをdivでwrapして返すコンポーネントを作成（粒度はatomic designで）
+ * pagesにmessageを表示させるためのページであるShowMessageを作成
+ * 一番上からはShowMessageのみを読み込み、Messageコンポーネントへのデータの受け渡しなどはShowMessageコンポーネント内で行う
+ * この形を取ることで、一番上のコンポーネントでは分離されたviewを読み込むだけ、それぞれのコンポーネントへのデータ処理はpages以下に分離できるのですっきりする
+ * pagesに関しては本来はURL単位などで作るといいと思う 実際のShowMessageはatomic designでいうところのorganismsの単位になるかな
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <ShowMessage />
+    </main>
   );
 }
 
